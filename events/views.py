924d8d109115d1a0bafa697a12f338d85e6efc3f
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-10 10:00:04 +0300

    updated calender dictionary

--- events/views.py
+++ events/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
 
 
 def home(request, year, month):
     name = "Timz Owen"
+    month_number = list(calendar.month_name).index(month)
     return render(request, "home.html", {
         'name': name,
         'year': year,
         'month': month,
+        'month_number': month_number,
     })
