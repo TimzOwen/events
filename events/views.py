34b5fc37a572f87fe2ab72f12dce35c2e7522560
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-10 22:49:11 +0300

    updated calender

--- events/views.py
+++ events/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
+from datetime import datetime
 
 
 def home(request, year, month):
-    name = "Timz Owen"
+    name = "Mr Owen"
+    month = month.capitalize()
     month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # create a calendar format
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    now = datetime.now()
+    current_year = now.year
+
+    time = now.strftime('%I:%M %p')
     return render(request, "home.html", {
         'name': name,
         'year': year,
         'month': month,
         'month_number': month_number,
+        'cal': cal,
+        'current_year':current_year,
+        'time':time
+
     })
