2642ee2e3482d5c7cd1f383ae15b5918185f6f9f
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-11 23:41:09 +0300

    modifying the admin module

--- events/admin.py
+++ events/admin.py
@@ -3,8 +3,26 @@ from .models import MyClubUsers
 from .models import Event
 from .models import Venue
 
-
 admin.site.register(MyClubUsers)
 admin.site.register(Event)
-admin.site.register(Venue)
 
+
+# admin.site.register(Venue)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'zip_code', 'phone', 'web,''email_address')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name','manager','event_date')
+    ordering = ('name',)
+    search_fields = ('name','manager')
+
+@admin.register(MyClubUsers)
+class MyClubAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name')
+    ordering = ('first_name',)
+    search_fields = ('first_name',)
--- events/models.py
+++ events/models.py
@@ -5,9 +5,9 @@ class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=120)
     zip_code = models.CharField('Zip Code', max_length=120)
-    phone = models.CharField('Contact Phone', max_length=120)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email address')
+    phone = models.CharField('Contact Phone', max_length=120,blank=True)
+    web = models.URLField('Website Address',blank=True)
+    email_address = models.EmailField('Email address',blank=True)
 
     def __str__(self):
         return self.name
@@ -25,11 +25,7 @@ class MyClubUsers(models.Model):
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=255)
     event_date = models.DateTimeField('Event date')
-    # connect two tables
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    # Null and Blank incase no table exits (Foreign connects
-    # 1->Many tables
-    # venue = models.CharField(max_length=120)
     manager = models.CharField(max_length=120)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUsers, blank=True)  # manyToMany connects several events
