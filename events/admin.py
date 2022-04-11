f6e67700c068119ad4b94ec4d6d86f6868bdc095
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-11 18:35:49 +0300

    updated db and migrations

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MyClubUsers
+from .models import Event
+from .models import Venue
 
-# Register your models here.
+
+admin.site.register(MyClubUsers)
+admin.site.register(Event)
+admin.site.register(Venue)
+
--- events/models.py
+++ events/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=120)
+    address = models.CharField(max_length=120)
+    zip_code = models.CharField('Zip Code', max_length=120)
+    phone = models.CharField('Contact Phone', max_length=120)
+    web = models.URLField('Website Address')
+    email_address = models.EmailField('Email address')
+
+    def __str__(self):
+        return self.name
+
+
+class MyClubUsers(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    email = models.EmailField('User Email')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=255)
+    event_date = models.DateTimeField('Event date')
+    # connect two tables
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    # Null and Blank incase no table exits (Foreign connects
+    # 1->Many tables
+    # venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(MyClubUsers, blank=True)  # manyToMany connects several events
+
+    def __str__(self):
+        return self.name
