299ec852eafcbddea5e2099b60fac0e97ef383d4
TimzOwen <ktimothy@kabarak.ac.ke>
2022-04-12 09:55:52 +0300

    updated venue front-end

--- events/forms.py
+++ events/forms.py
@@ -7,3 +7,19 @@ class VenueForm(ModelForm):
     class Meta:
         model = Venue
         fields = ('name', 'address', 'zip_code', 'phone', 'web', 'email_address')
+        labels = {
+            'name': '',
+            'address': '',
+            'zip_code': '',
+            'phone': '',
+            'web': '',
+            'email_address': '',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control','placeholder':'Event Venue'}),
+            'address': forms.TextInput(attrs={'class': 'form-control','placeholder':'Event Address'}),
+            'zip_code': forms.TextInput(attrs={'class': 'form-control','placeholder':'Event Zip Code'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control','placeholder':'Organizer Phone number'}),
+            'web': forms.TextInput(attrs={'class': 'form-control','placeholder':'Web URL Event'}),
+            'email_address': forms.EmailInput(attrs={'class': 'form-control','placeholder':'Email to event'}),
+        }
--- events/views.py
+++ events/views.py
@@ -4,10 +4,21 @@ from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
 from .forms import VenueForm
+from django.http import HttpResponseRedirect
 
 
 def add_venue(request):
-    return render(request, 'events/add_venue.html', {})
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'events/add_venue.html', {'form': form, 'submitted': submitted})
 
 
 def all_events(request):
