0cc1407da1ee1ee310a614eeae655cf940ec004f
TimzOwen <timzowen@gmail.com>
2022-04-17 18:20:06 +0300

    updated the event page updates

--- events/views.py
+++ events/views.py
@@ -21,6 +21,15 @@ def add_event(request):
     return render(request, 'events/add_event.html', {'form': form, 'submitted': submitted})
 
 
+def update_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    form = EventForm(request.POST or None, instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('list-venues')
+    return render(request, 'events/update_event.html', {'event': event, 'form': form})
+
+
 def update_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
     form = VenueForm(request.POST or None, instance=venue)
