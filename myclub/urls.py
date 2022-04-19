2147db6e1f76d9781d84e28fd14f0dec9ca507e1
TimzOwen <timzowen@gmail.com>
2022-04-19 11:57:42 +0300

    admin customize

--- myclub/urls.py
+++ myclub/urls.py
@@ -4,4 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls'))
+
 ]
+
+# configure Admin Titles
+admin.site.site_header = "Club Admins Page"
+admin.site.site_title = "Events "
+admin.site.index_title="Manage The events "
