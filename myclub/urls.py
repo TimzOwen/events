4b6bc48a4ea254bd99f8e7262b58e0e8d8b16197
TimzOwen <timzowen@gmail.com>
2022-04-19 12:06:56 +0300

    updated login app

--- myclub/urls.py
+++ myclub/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls'))
+    path('', include('events.urls')),
+    path('members/', include("django.contrib.auth.urls")),
+    path('members/', include('members.urls'))
 
 ]
 
 # configure Admin Titles
 admin.site.site_header = "Club Admins Page"
 admin.site.site_title = "Events "
-admin.site.index_title="Manage The events "
+admin.site.index_title = "Manage The events "
