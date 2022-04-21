46d52709cc46d0f8c5dd905c77e72a866bb03582
TimzOwen <timzowen@gmail.com>
2022-04-21 23:13:37 +0300

    updated path

--- myclub/settings.py
+++ myclub/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'myclub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
