37916f01eccc38997ecac2de4233ee39311cec58
TimzOwen <timzowen@gmail.com>
2022-04-25 09:41:08 +0300

    payments update

--- events/models.py
+++ events/models.py
@@ -9,6 +9,7 @@ class Venue(models.Model):
     phone = models.CharField('Contact Phone', max_length=120, blank=True)
     web = models.URLField('Website Address', blank=True)
     email_address = models.EmailField('Email address', blank=True)
+    owner = models.IntegerField("venue owner",blank=False,default=1)
 
     def __str__(self):
         return self.name
